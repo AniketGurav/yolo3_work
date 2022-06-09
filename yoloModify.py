a9310d9b595430403ff8d2563d61727077b2814f
AniketGurav <aniket.diat@gmail.com>
2022-06-09 18:02:14 +0200

    Update yoloModify.py

--- yoloModify.py
+++ yoloModify.py
@@ -1,3 +1,7 @@
+"""
+    This is code for toy example localization with PHOSC in it
+"""
+
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.layers import Conv2D, Input, LeakyReLU, ZeroPadding2D, BatchNormalization, MaxPool2D
